311e1385e9ace9b3739b3970e784b5abe69a1091
Daniel Morel <dxm190015@utdallas.edu>
2023-12-05 20:35:55 -0600

    Base functionality in place!

--- Assets/Editor/AssetCloner.cs
+++ Assets/Editor/AssetCloner.cs
@@ -41,4 +41,33 @@ public class AssetCloner : EditorWindow
             CloneAsset();
         }
     }
+
+    //Create the cloner function
+    private void CloneAsset()
+    {
+        //throw error if trying to clone but field is empty
+        if (_assetToClone == null)
+        {
+            Debug.LogError("Error: Please Assign an Asset to Clone.");
+            return;
+        }
+
+        //throws error if name field is empty
+        if (_assetName == string.Empty)
+        {
+            Debug.LogError("Error: Please Assign an Asset Name.");
+            return;
+        }
+
+        //here we set up the random spawns
+        Vector2 _spawnZone = Random.insideUnitCircle * _assetSpawnZone; //randomly selects spawn positions within zone
+        Vector3 _spawnPos = new Vector3(_spawnZone.x, 0f, _spawnZone.y); //positions asset at those^ coords
+
+        //this bit creates the clone to place at location determined above
+        GameObject _clonedAsset = Instantiate(_assetToClone, _spawnPos, Quaternion.identity);
+        _clonedAsset.name = _assetName + _assetNum; //renames clone
+        _clonedAsset.transform.localScale = Vector3.one * _assetScale; //uses scale slide to determine asset size on spawn
+
+        _assetNum++; //increases assetNum each time button is pressed
+    }
 }
